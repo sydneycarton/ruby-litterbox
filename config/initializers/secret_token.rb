195a9540bf979a20ca9d65ac46c9671bc5765bb2
Kea Johnston <sydneycarton@gmail.com>
2013-02-06 19:49:07 -0800

    blew away the project

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MostlyStatic::Application.config.secret_token = 'dea089328b7be3073e0327d0cf6d522b5ce72bc07ff0aaca8c07fe40bea93ed1b05863fe67d5c9f298ecf29fa8239ff87a1078d02073fbfda97443a5fa7a8f57'
