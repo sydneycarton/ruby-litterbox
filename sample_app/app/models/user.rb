02e5637042cf4a7f54452b10e66e4d98d01cc26f
Kea Johnston <sydneycarton@gmail.com>
2013-03-03 17:33:42 -0800

    Did exercises for chapter 7

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   	user.email=email.downcase
   }
   validates :password, :presence=>true, :length=>{:minimum=>6}
-  validates :password_confirmation, :presence=>true
+  #validates :password_confirmation, :presence=>true
   validates :name, :presence=>true, :length=>{:maximum=>50}
   VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence=>true, :format=>{:with=>VALID_EMAIL_REGEX}, :uniqueness=>{ :case_sensitive=>false }
